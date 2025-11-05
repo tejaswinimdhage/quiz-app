fa7a581afd7afa0350ada05444f30612db5d2a22
tejaswini <tejaswini.dhage@simplecrm.com>
2025-11-06 01:25:52 +0530

    route fix added

--- frontend/src/components/QuizList/index.jsx
+++ frontend/src/components/QuizList/index.jsx
@@ -35,7 +35,7 @@ const QuizList = () => {
                 {quizzes.map((quiz) => (
                     <Grid item key={quiz._id} xs={12} sm={6} md={4} lg={3}>
                         <Card
-                            component={Link} to={`/quizzes/${quiz._id}`}
+                            component={Link} to={`/${quiz._id}`}
                             sx={{
                                 height: 150,
                                 display: "flex",
